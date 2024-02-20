498d56c649dc2cb740c9394b76f518b5465d6573
joey <qpengf@gmail.com>
2024-02-20 16:30:10 +0800

    9 * 9

--- Basic/src/com/suan/sequences/WhileDemo01.java
+++ Basic/src/com/suan/sequences/WhileDemo01.java
@@ -1,12 +1,16 @@
 package com.suan.sequences;
 
-public class ForDemo04 {
+public class WhileDemo01 {
     public static void main(String[] args) {
-        for (int i = 1; i <= 9; i++) {
-            for (int j = 1; j <= i; j++) {
+        int i = 1;
+        while (i <=9){
+            int j = 1;
+            while (j <= i){
                 System.out.print(j + " * " +i +" = "+ i*j + "\t");
+                j++;
             }
             System.out.println();
+            i++;
         }
     }
 }
