5b95286651ecd9c2840595780eefa153a98d34b9
joey <qpengf@gmail.com>
2024-02-27 09:18:08 +0800

    sparseArray

--- Basic/src/com/suan/array/Demo08.java
+++ Basic/src/com/suan/array/Demo08.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class Demo08 {
     public static void main(String[] args) {
+        System.out.println("原始二维数组：");
         int[][] arrays = new int[11][11];
         arrays[1][2] = 1;
         arrays[2][3] = 2;
@@ -25,12 +26,12 @@ public class Demo08 {
             }
         }
 
-        System.out.println(value);
-
-        int [][] spareArray = new int[value+1][3];
-        spareArray[0][0] = 11;
-        spareArray[0][1] = 11;
-        spareArray[0][2] = value;
+//        System.out.println(value);
+        System.out.println("压缩二维数组：");
+        int [][] sparseArray = new int[value+1][3];
+        sparseArray[0][0] = 11;
+        sparseArray[0][1] = 11;
+        sparseArray[0][2] = value;
 
         int count = 0;
 
@@ -38,13 +39,13 @@ public class Demo08 {
             for (int j = 0; j < arrays[i].length; j++) {
                 if (arrays[i][j] != 0){
                     count ++;
-                    spareArray[count][0] = i;
-                    spareArray[count][1] = j;
-                    spareArray[count][2] = arrays[i][j];
+                    sparseArray[count][0] = i;
+                    sparseArray[count][1] = j;
+                    sparseArray[count][2] = arrays[i][j];
                 }
             }
         }
-        for (int[] ints : spareArray) {
+        for (int[] ints : sparseArray) {
             for (int anInt : ints) {
                 System.out.print(anInt + "\t");
 
@@ -53,6 +54,16 @@ public class Demo08 {
 
         }
         System.out.println("还原的二维数组：");
+        int [][] restoreArray = new int [sparseArray[0][0]][sparseArray[0][1]];
+        for (int i = 1; i < sparseArray.length; i++) {
+            restoreArray[sparseArray[i][0]][sparseArray[i][1]] = sparseArray[i][2];
+        }
+        for (int[] ints : restoreArray) {
+            for (int anInt : ints) {
+                System.out.print(anInt + "\t");
+            }
+            System.out.println(" ");
+        }
 
     }
 }
