38cd9025fbff4b4d9b58e09a5ce24de1e1baa9e0
joey <qpengf@gmail.com>
2024-03-04 09:34:01 +0800

    Num32

--- Basic/src/com/oop/Demo05/Num32.java
+++ Basic/src/com/oop/Demo05/Num32.java
@@ -6,10 +6,24 @@ public class Num32 {
             String bit = ((num & (1 << i)) == 0) ? "0":"1";
             System.out.print(bit);
         }
+        System.out.println();
     }
 
     public static void main(String[] args) {
-        int num = 8;
+        int num = -5;
         print(num);
+
+        int a = Integer.MAX_VALUE;
+        System.out.println(a);
+        int b = 100;
+        int c = 200;
+        System.out.println("========");
+        print(b);
+        print(c);
+        System.out.println("========");
+        print(b & c);
+        print(b | c);
+        print(b ^ c);
+
     }
 }
